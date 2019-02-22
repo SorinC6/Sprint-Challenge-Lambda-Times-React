2957f6a140d5cea64a4796840b84e71af2bbfedb
Sorin Chis <kish.sorin@yahoo.com>
2019-02-22 11:05:32 +0200

    PropTypes added to Tab and Tabs compoenents

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -14,5 +14,6 @@ const Cards = (props) => {
 };
 
 // Make sure you include prop types for all of your incoming props
-
 export default Cards;
+
+
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Tab from './Tab';
+import PropTypes from 'prop-types';
 const Tabs = (props) => {
 	return (
 		<div className="tabs">
@@ -7,8 +8,15 @@ const Tabs = (props) => {
 				<span className="title">TRENDING TOPICS:</span>
 				{/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
-				{props.tabs.map((tab,i) => {
-					return <Tab key={i} tab={tab} selectedTab={props.selectedTab} selectTabHandler={props.selectTabHandler}/>;
+				{props.tabs.map((tab, i) => {
+					return (
+						<Tab
+							key={i}
+							tab={tab}
+							selectedTab={props.selectedTab}
+							selectTabHandler={props.selectTabHandler}
+						/>
+					);
 				})}
 			</div>
 		</div>
@@ -17,3 +25,9 @@ const Tabs = (props) => {
 
 // Make sure to use PropTypes to validate your types!
 export default Tabs;
+
+Tabs.propTypes = {
+	tab: PropTypes.string,
+	selectedTab: PropTypes.string,
+	selectTabHandler: PropTypes.func
+};
